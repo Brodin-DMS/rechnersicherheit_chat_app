6f06041bee208f56bc48bb26473a7b91e560c11c
Brodin-DMS <strauss_dierk@protonmail.com>
2021-05-17 21:56:39 +0200

    messages now work with username instead of id, groupchats are working, input dialog inproved

--- ClineatServerPackage/MessagePackage/Message.py
+++ ClineatServerPackage/MessagePackage/Message.py
@@ -1,55 +1,76 @@
 from abc import ABCMeta
 
+
 class BaseMessage(metaclass=ABCMeta):
-    def __init__(self, content, senderId):
+    def __init__(self, content):
         self.content = content
-        self.senderId = senderId
         self.messageType = None
 
-#class PrivateTextMessage(BaseMessage):
-class PrivateTextMessage():
-    @staticmethod
-    def Create(content, senderId, receiverId):
-        return PrivateTextMessage(content, senderId, receiverId)
 
-    def __init__(self, content, senderId, receiverId):
-        #super().__init__(content, senderId)
-        self.content = content
-        self.senderId = senderId
-        self.receiverId = receiverId
+class PrivateTextMessage(BaseMessage):
+    @staticmethod
+    def create(content, sender_name, receiver_name):
+        return PrivateTextMessage(content, sender_name, receiver_name)
+
+    def __init__(self, content, sender_name, receiver_name):
+        super().__init__(content)
+        self.sender_name = sender_name
+        self.receiver_name = receiver_name
         self.messageType = MessageType.PrivateTextMessage
+
 
 class GroupTextMessage(BaseMessage):
     @staticmethod
-    def Create(content, senderId, groupId):
-        return GroupTextMessage(content, senderId, groupId)
+    def create(content, sender_name, receiver_name):
+        return GroupTextMessage(content, sender_name, receiver_name)
 
-    def __init__(self, content, senderId, groudId):
-        super().__init__(content, senderId)
-        self.groupId = groudId
+    def __init__(self, content, sender_name, receiver_name):
+        super().__init__(content)
+        self.sender_name = sender_name
+        self.receiver_name = receiver_name
         self.messageType = MessageType.GroupTextMessage
+
 
 class LoginMessage:
     @staticmethod
-    def Create(username, password, sender_id):
-        return LoginMessage(username, password,sender_id)
+    def create(username, password):
+        return LoginMessage(username, password)
 
-    def __init__(self, username, password, senderId):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.senderId = senderId
         self.messageType = MessageType.LoginMessage
 
 
-class LoginResponse:
+class MessageResponse:
     @staticmethod
-    def Create(code):
-        return LoginResponse(code)
-
+    def create(code):
+        return MessageResponse(code)
 
     def __init__(self, code):
         self.responseCode = code
         self.messageType = MessageType.LoginResponse
+
+
+class SignUpMessage:
+    @staticmethod
+    def create(username, password):
+        return SignUpMessage(username, password)
+
+    def __int__(self, username, password):
+        self.username = username
+        self.password = password
+        self.messageType = MessageType.SignUpMessage
+
+
+class CreateGroupMessage:
+    @staticmethod
+    def create(username, group_name):
+        return CreateGroupMessage(username, group_name)
+
+    def __init__(self, username, group_name):
+        self.username = username
+        self.group_name = group_name
 
 
 class MessageType:
@@ -57,3 +78,5 @@ class MessageType:
     GroupTextMessage = 1
     LoginMessage = 2
     LoginResponse = 3
+    SignUpMessage = 4
+    CreateGroupMessage = 5
